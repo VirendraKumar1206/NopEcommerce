b3386c899c4e60272e4a024d5895ef891e8a0220
Virendra kumar <159077101+VirendraKumar1206@users.noreply.github.com>
2024-07-24 14:29:39 +0530

    Update Extract.java

--- src/test/java/NopCommerce_Project/Extract.java
+++ src/test/java/NopCommerce_Project/Extract.java
@@ -8,54 +8,46 @@ import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 
-public class Extract {
-	//It returns a two-dimensional array of Objects (Object[][]).
-	public Object[][] getdatafromexel() throws EncryptedDocumentException, IOException {
-		FileInputStream file = new FileInputStream(//here we creating the an object of fileinputStream class and giving it excel file location as its para meter
-				"C:\\Users\\HP\\Documents\\workspace-spring-tool-suite-4-4.21.0.RELEASE\\Assign_project\\src\\test\\resources\\project1.xlsx");
-
-		/*
-		 * here we are creating a new object of FileInputStream class, named file , to
-		 * read the data from the Excel file located at the specified path.
-		 * 
-		 */
-
-		Workbook book = WorkbookFactory.create(file);
-
-		/*
-		 * here we have created a Workbook object (book) by using the
-		 * WorkbookFactory.create() method, which takes the FileInputStream (file) as
-		 * input. This allows the program to read and manipulate the contents of the
-		 * Excel file.
-		 */
-
-		Sheet sheet = book.getSheet("Sheet1");
-
-		short Cellsize = sheet.getRow(0).getLastCellNum();
-		                             /*
-									 * 
-									 * here we are retrieving the number of cells (columns) present in the first row of
-									 * an Excel sheet and stores that value in a variable named Cellsize.
-									 *
-									 */
-
-		int rowcount = sheet.getLastRowNum(); /*
-												 * here we are getting the total number of rows in an Excel spreadsheet
-												 * and stores that value in a variable named rowcount.
-												 */
-		Object[][] obj = new Object[rowcount][Cellsize];
-
-		/*
-		 * here we are creating an object and initializes a two-dimensional array obj to
-		 * store data retrieved from the Excel file,
-		 */
-
-		for (int i = 0; i < rowcount; i++) {
-			for (int j = 0; j < Cellsize; j++) {
-				obj[i][j] = sheet.getRow(i).getCell(j).getStringCellValue();
+public class DataExtract {
+	public Object[][] getdatafromexcel() throws EncryptedDocumentException, IOException {
+		// getting data from excel and storing it
+		
+		FileInputStream fis =  new FileInputStream("C:\\Users\\admin\\Documents\\workspace-spring-tool-suite-4-4.21.0.RELEASE\\selenium_project\\src\\main\\resources\\Register.xlsx");
+		
+		Workbook book = WorkbookFactory.create(fis);
+		
+		  // mention sheet name 
+	    Sheet sh = book.getSheet("TMKOC");
+	      //getting last row and last col
+	 
+         short cellsize = sh.getRow(0).getLastCellNum();//for col
+         int rowsize = sh.getLastRowNum();//for row
+         
+         Object [][] obj = new Object[rowsize][cellsize];
+         
+         for (int i = 0; i < rowsize; i++) {
+			for (int j = 0; j < cellsize; j++) {
+				
+				obj[i][j] = sh.getRow(i+1).getCell(j).getStringCellValue();
 			}
 		}
 		return obj;
-
+		
 	}
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
